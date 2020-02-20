1af7a67ebb7bea9513930e6724bcd99361ec627b
cjiles <57699863+cjiles@users.noreply.github.com>
2020-02-20 01:03:07 -0600

    Created App and Components

---
+++ shared/locations.js
@@ -0,0 +1,30 @@
+export const LOCATIONS = [
+    {
+        id: 0,
+        name:'New Orleans',
+        image: './images/NewOrleans.jpg',
+        altimage: "New Orleans, Louisiana",
+        address: "3200 Canale St.",
+        hours: "7 AM - 7 PM",
+        number: "555-555-5555"
+    },
+    {
+        id: 1,
+        name:'Omaha',
+        image: './images/Omaha.jpg',
+        altimage: "Omaha, Nebraska",
+        address: "605 Riverfront Dr",
+        hours: "6 AM - 9 PM",
+        number: "555-555-5555"
+        
+    },
+    {
+        id: 2,
+        name:'Memphis',
+        image: './images/Memphis.jpg',
+        altimage: "Memphis, TN",
+        address: "555 Main St.",
+        hours: "7 AM - 7 PM",
+        number: "555-555-5555"
+    }
+]
