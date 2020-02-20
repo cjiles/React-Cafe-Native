c3d4acac34ce7fd471f43487848803f6953e7ffb
cjiles <57699863+cjiles@users.noreply.github.com>
2020-02-20 01:12:51 -0600

    Created Components

---
+++ components/HomeComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Home extends Component {
+
+}
+
+export default Home;
---
+++ components/MainComponent.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { createStackNavigator } from 'react-navigation';
+
+class Main extends Component{
+
+}
+
+export default Main;
